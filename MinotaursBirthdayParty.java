35d7bf747aee38afc43f88b7412b8fe3d4d1cd51
Brian Moon <apollomoon@knights.ucf.edu>
2022-03-03 14:25:24 -0500

    updated version without printing in threads, need to do this to ensure no race conditions are happening without being seen due to the sync nature of printing

--- MinotaursBirthdayParty.java
+++ MinotaursBirthdayParty.java
@@ -1,7 +1,7 @@
 // Brian Moon
 // COP4520
-// PA2
-// MinotaursBirthdayParty
+// PA2 - Problem 1
+// MinotaursBirthdayParty.java
 
 import java.util.Random;
 import java.util.Set;
@@ -15,7 +15,8 @@ public class MinotaursBirthdayParty extends Thread {
   public static int numGuests;
   private int currentGuestNumber;
   private AtomicBoolean hasEatenCupcake = new AtomicBoolean(false);
-  // private AtomicBoolean selected = new AtomicBoolean(false);
+  // private AtomicBoolean hasEatenCupcake = new AtomicBoolean(false);
+  private AtomicBoolean selected = new AtomicBoolean(false);
   public static int numOfIterationsRequired = 0;
   public static AtomicInteger counter = new AtomicInteger(1);
   public static AtomicBoolean cupcake = new AtomicBoolean(true);
@@ -23,7 +24,7 @@ public class MinotaursBirthdayParty extends Thread {
   public static Set<Integer> set = new HashSet<Integer>();
   
   // static boolean[] hasEatenCupcake = new boolean[numGuests];
-  static boolean[] isSelected = new boolean[100];
+  // static boolean[] isSelected = new boolean[100];
 
   MinotaursBirthdayParty(int guestNumber) {
     this.currentGuestNumber = guestNumber;
@@ -31,33 +32,32 @@ public class MinotaursBirthdayParty extends Thread {
 
   // run method
   public void run() {
-    
-    // this.hasEatenCupcake = false;
     while (counter.get() < numGuests) {
       tryToEnterLabyrinth();
     }
   }  
 
   public void tryToEnterLabyrinth() {
-    if (isSelected[this.currentGuestNumber] == false) {
-      isSelected[this.currentGuestNumber] = true;
+    if (this.selected.get() == true) {
+      this.selected.set(false);
       enterLabyrinth();
     }
   }
     
   // The guest enters Minotaur's Labyrinth
   public synchronized void enterLabyrinth() {
+    
     lock.lock();
     try {
       // set.add(this.currentGuestNumber);
       if (this.currentGuestNumber == 0 && cupcake.get() == false) {
         counter.getAndIncrement();
         cupcake.set(true);
-        System.out.println("The captain increments the count and requests a new yummy cupcake!"); 
+        // System.out.println("The captain increments the count and requests a new yummy cupcake!"); 
       } else if (this.hasEatenCupcake.get() == false && cupcake.get() == true && this.currentGuestNumber != 0) {
         this.hasEatenCupcake.set(true);
         cupcake.set(false);
-        System.out.println("Hungry Guest Number " + this.currentGuestNumber + " has eaten a cupcake!");
+        // System.out.println("Hungry Guest Number " + this.currentGuestNumber + " has eaten a cupcake!");
       }
     } catch(Exception e) {
         e.printStackTrace();
@@ -81,15 +81,17 @@ public class MinotaursBirthdayParty extends Thread {
       minotaursGuests[guestNumber] = new MinotaursBirthdayParty(guestNumber);
       minotaursGuests[guestNumber].start();
     }
+    System.out.println("Loading...");
     // Minotaur picks random guests for labyrinth
     while (counter.get() < numGuests) {
-      // System.out
+      
       int randomGuest = random.nextInt(numGuests);
       
-      // minotaursGuests[randomGuest].selected.set(true);
-      isSelected[randomGuest] = false;
+      minotaursGuests[randomGuest].selected.set(true);
+      // isSelected[randomGuest] = false;
       // System.out.println("Guest Selected By Minotaur: " + randomGuest);
       // minotaursGuests[randomGuest].enterLabyrinth();
+      
       // join threads/guests
       if (counter.get() == numGuests) {
         for (int guestNumber = 0; guestNumber < numGuests; ++guestNumber) {
@@ -99,10 +101,9 @@ public class MinotaursBirthdayParty extends Thread {
           } catch(Exception e) { e.printStackTrace(); }
         }
       }
-      ++numOfIterationsRequired;
     }
-    // System.out.println(numOfIterationsRequired + " iterations required for " + numGuests + " guests.");
-    System.out.println("Guest Count: " + counter);
+    System.out.println("Loading Complete!");
+    System.out.println("All " + counter + " guests have made it through the Minotaur's Labyrinth!");
     long endTime = System.currentTimeMillis();
     System.out.println("Execution Time: " + (endTime - startTime) + "ms");
     // for (int item : set) {
